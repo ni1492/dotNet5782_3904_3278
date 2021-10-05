99eab006f935a63c0ca8a7309f49cb6acdc1876a
nofriedm <nofriedm@g.jct.ac.il>
2021-10-05 23:23:04 +0300

    I added a new welcome function.

---
+++ Targil0/Program3904.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Targil0
+{
+    partial class Program
+    {
+        static partial void Welcome3904()
+        {
+            Console.Write("Enter your name: ");
+            string name = Console.ReadLine();
+            Console.WriteLine("{0} welcome to *MY* first console application!!! (I am also here!!!)", name);
+
+        }
+    }
+}
