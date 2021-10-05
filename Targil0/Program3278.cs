968dfe47ad4831ddbd0d3b00197aa6d85e4b4347
ni1492 <nilan@g.jct.ac.il>
2021-10-05 23:05:51 +0300

    initial code and Welcome functions קוד התחלתי+ פונקציות ברוכים הבאים

---
+++ Targil0/Program3278.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Targil0
+{
+    partial class Program
+    {
+        static void Main(string[] args)
+        {
+            Welcome3278();
+            Welcome3904();
+            Console.ReadKey();
+        }
+        static partial void Welcome3904();
+        private static void Welcome3278()
+        {
+            Console.Write("Enter your name: ");
+            string userName = Console.ReadLine();
+            Console.WriteLine("{0}, welcome to my first console application", userName);
+        }
+    }
+}
